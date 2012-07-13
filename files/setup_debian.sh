eee795f7249ec2b7cc1eafe83fa8cd87f9b32454
Robert Schulze <robert@dotless.de>
2012-07-13 20:06:36 +0200

    `apt-get update` only when initial install fails.
    Usually `apt-get install` should just run fine,
    on some systems apt's cache may be deleted for
    the sake of a smaller basebox size.
    However, we need a workung apt cache to install
    some packages.
    We'll just try to install the packes, if this,
    for what ever reason fails, `apt-get update`
    is let losse whith all possible side effects
    on package dependencies. When `update` runs
    successfully we try `install` again. When
    either fails, we'll stop and return an error.
    
    (thanks @dazz and @cmile for pointing)

--- files/setup_debian.sh
+++ files/setup_debian.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-apt-get update
-apt-get install -y linux-headers-`uname -r` dkms || exit 1
+function do_install {
+	apt-get install -y linux-headers-`uname -r` dkms
+}
+
+do_install || (apt-get update && do_install) || exit 1
+
 mount /tmp/VBoxGuestAdditions.iso -o loop /mnt
 /mnt/VBoxLinuxAdditions.run --nox11
 umount /mnt
+
+exit 0
