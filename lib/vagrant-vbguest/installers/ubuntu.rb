ec9a7b1f0a33ab9e78d5d6bdbbaad3a3ae2104e3
Robert Schulze <robert@dotless.de>
2013-10-20 20:32:41 +0200

    Cope with UbuntuCloudImage by default
    On Ubuntu, always try to remove conflicting installations of GuestAdditions by removing vm packages: virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11

--- lib/vagrant-vbguest/installers/ubuntu.rb
+++ lib/vagrant-vbguest/installers/ubuntu.rb
@@ -6,6 +6,19 @@ module VagrantVbguest
         :ubuntu == self.distro(vm)
       end
 
+      def install(opts=nil, &block)
+        remove_packaged_additions(opts=nil, &block)
+        super
+      end
+
+    protected
+
+      def remove_packaged_additions(opts=nil, &block)
+        options = (opts || {}).merge(:error_check => false)
+        command = "dpkg --list | grep virtualbox-guest && apt-get -y -q purge virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11"
+        communicate.sudo(command, options, &block)
+      end
+
     end
   end
 end
