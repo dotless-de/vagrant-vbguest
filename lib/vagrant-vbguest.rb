f7031775073cd1f51fe5128b3f6f817210da40d2
Alex Rehsitd <qblake.public@gmail.com>
2013-10-21 17:49:00 +0000

    add lazy load for some classes

--- lib/vagrant-vbguest.rb
+++ lib/vagrant-vbguest.rb
@@ -10,21 +10,28 @@ I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
 require "vagrant-vbguest/errors"
 require 'vagrant-vbguest/vagrant_compat'
 
-require 'vagrant-vbguest/machine'
-
-require 'vagrant-vbguest/hosts/base'
-require 'vagrant-vbguest/hosts/virtualbox'
-
-require 'vagrant-vbguest/installer'
-require 'vagrant-vbguest/installers/base'
-require 'vagrant-vbguest/installers/linux'
-require 'vagrant-vbguest/installers/debian'
-require 'vagrant-vbguest/installers/ubuntu'
-require 'vagrant-vbguest/installers/redhat'
-
-require 'vagrant-vbguest/middleware'
 
 module VagrantVbguest
+  autoload :Machine,    'vagrant-vbguest/machine'
+
+  module Hosts
+    autoload :Base,       'vagrant-vbguest/hosts/base'
+    autoload :VirtualBox, 'vagrant-vbguest/hosts/virtual_box'
+  end
+
+  autoload :Installer,  'vagrant-vbguest/installer'
+
+  module Installers
+    autoload :Base,   'vagrant-vbguest/installers/base'
+    autoload :Linux,  'vagrant-vbguest/installers/linux'
+    autoload :Debian, 'vagrant-vbguest/installers/debian'
+    autoload :Redhat, 'vagrant-vbguest/installers/redhat'
+    autoload :Ubuntu, 'vagrant-vbguest/installers/ubuntu'
+  end
+  autoload :Middleware, 'vagrant-vbguest/middleware'
+
+  autoload :Config, 'vagrant-vbguest/config'
+  autoload :CommandCommons, 'vagrant-vbguest/command'
 
   class Plugin < Vagrant.plugin("2")
 
@@ -35,7 +42,7 @@ module VagrantVbguest
     DESC
 
     config('vbguest') do
-      require File.expand_path("../vagrant-vbguest/config", __FILE__)
+      # require File.expand_path("../vagrant-vbguest/config", __FILE__)
       Config
     end
 
