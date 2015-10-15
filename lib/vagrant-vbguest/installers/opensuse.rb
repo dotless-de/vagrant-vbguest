11765f2c18578ec204de2968284b90dbbd80468b
Robert Schulze <robert@dotless.de>
2015-10-15 16:26:43 +0200

    add a opensuse implementation based on #163
    VirtualBox Installer seems not work though:
    Uncompressing VirtualBox 5.0.2 Guest Additions for Linux./mnt/VBoxLinuxAdditions.run: line 137: /dev/tty: No such device or address

---
+++ lib/vagrant-vbguest/installers/opensuse.rb
@@ -0,0 +1,32 @@
+module VagrantVbguest
+  module Installers
+    class OpenSuse < Linux
+      # OpenSuse shows up as "suse", check for presence of the zypper
+      # package manager as well.
+      def self.match?(vm)
+        :suse == self.distro(vm) && has_zypper?(vm)
+      end
+
+      # Install missing deps and yield up to regular linux installation
+      def install(opts=nil, &block)
+        communicate.sudo(install_dependencies_cmd, opts, &block)
+        super
+      end
+
+    protected
+      def self.has_zypper?(vm)
+        communicate_to(vm).test "which zypper"
+      end
+
+      def install_dependencies_cmd
+        "zypper --non-interactive install #{dependencies}"
+      end
+
+      def dependencies
+        packages = ['kernel-devel', 'gcc', 'make']
+        packages.join ' '
+      end
+    end
+  end
+end
+VagrantVbguest::Installer.register(VagrantVbguest::Installers::OpenSuse, 5)
