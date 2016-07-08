bc41092ad6635f7ff5562fddeddb41949de74919
Victor Pereira <vpereira@suse.de>
2016-07-08 12:41:39 +0200

    check if suse version < 12

--- lib/vagrant-vbguest/installers/suse.rb
+++ lib/vagrant-vbguest/installers/suse.rb
@@ -3,31 +3,48 @@ module VagrantVbguest
     class Suse < Linux
       # To distingish between OpenSuse and SLEs (both shows up as "suse"),
       # check for presence of the zypper and entry on os-release
+
       def self.match?(vm)
         :suse == self.distro(vm) && has_zypper?(vm) && sles?(vm)
       end
 
       # Install missing deps and yield up to regular linux installation
       def install(opts=nil, &block)
-        communicate.sudo(install_dependencies_cmd, opts, &block)
+        case sles_version
+        when 10..11.4
+              communicate.sudo(install_dependencies_cmd(pre_12=true), opts, &block)
+        when 12.0..13.0
+              communicate.sudo(install_dependencies_cmd, opts, &block)
+        end
         super
       end
 
     protected
       def self.sles?(vm)
-        communicate_to(vm).test "grep -q ID=\\\"sles\\\" /etc/os-release"
+        communicate_to(vm).test "grep -q ID\"sles\" /etc/os-release"
+      end
+
+      def self.sles_version(vm)
+        communicate_to(vm).sudo("cat /etc/os-release | grep VERSION_ID | cut -f2 -d'='", :error_check => false) do |t,d|
+          return d.to_f
+        end
       end
 
       def self.has_zypper?(vm)
         communicate_to(vm).test "which zypper"
       end
 
-      def install_dependencies_cmd
-        "zypper --non-interactive install #{dependencies}"
+      def install_dependencies_cmd(pre_12=false)
+        "zypper --non-interactive install #{dependencies(pre_12)}"
       end
 
-      def dependencies
-        packages =  ['kernel-devel', 'gcc', 'make', 'tar']
+
+      def dependencies(pre_12=false)
+        packages = if pre_12
+          ['kernel-default-devel', 'gcc', 'make', 'tar']
+        else
+          ['kernel-devel', 'gcc', 'make', 'tar']
+        end
         packages.join ' '
       end
     end
